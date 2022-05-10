cb48239f70a6f2b5a256415b80ee7b847db5a7fc
seunghyun <dkskakdgka@gmail.com>
2022-05-10 16:27:09 +0900

    update.js 수정

--- routes/article/update.js
+++ routes/article/update.js
@@ -7,17 +7,9 @@ module.exports = async function (app, opts) {
   app.put('/:id', async function (request, reply) {
     const result = await updateOne(this.mongo, request.params.id, request.body)
 
-    if(!result) {
-      reply
-      .code(404)
-      .header("content-type","application/json")
-      .send({error : "Not Found"})
-    }
-    else {
-      reply
-      .code(200)
-      .header("Content-Type","application/json")
-      .send({value : result})
-    }
+    reply
+    .code(200)
+    .header("Content-Type","application/json")
+    .send({value : result})
   })
 }
